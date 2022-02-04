ea77eb4c396b6f5ff65667a17d808cc3e5d3a4c0
Gioelinz <gioelerunza.gr@gmail.com>
2022-02-04 15:41:32 +0100

    add 10 mail genreated

--- js/script.js
+++ js/script.js
@@ -5,4 +5,25 @@ Vue.config.devtools = true;
 
 const app = new Vue({
     el: '#root',
+    data: {
+        emails: [],
+        totalGenEmails: 10,
+    },
+    methods: {
+        getRandEmail() {
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then(res => {
+                this.emails.push(res.data.response);
+            })
+        },
+
+        getRandEmails(number) {
+            for (let i = 0; i < number; i++) {
+                this.getRandEmail();
+            }
+        }
+    },
+
+    mounted() {
+        this.getRandEmails(this.totalGenEmails);
+    }
 });
